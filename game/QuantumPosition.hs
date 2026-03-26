a3426c9caf3698a79ab7be22be573195970880b1
silvermannn <227780093+silvermannn@users.noreply.github.com>
2026-03-26 22:06:35 +0300

    Removed unnecessary dependencies in QuantumPosition

--- game/QuantumPosition.hs
+++ game/QuantumPosition.hs
@@ -6,30 +6,26 @@ import Data.Foldable
 import Data.List
 import Data.Maybe
 
-import ClassicPosition
-import Matrix
 import Position
 
-data QuantumPosition = QuantumPosition
-  { boardSizeX :: Int
-  , boardSizeY :: Int
-  , classicPositions :: [ClassicPosition]
+data QuantumPosition pos = QuantumPosition
+  { positions :: [pos]
   , rnd :: StdGen
   } deriving (Show)
 
-data QuantumMove
-  = ClassicMove Coord
-  | QuantumMove Coord Coord
+data QuantumMove m
+  = ClassicMove m
+  | QuantumMove m m
   deriving (Show)
 
-data QuantumParams =
-  QuantumParams Int Int Int
+data QuantumParams par =
+  QuantumParams par Int
   deriving (Show)
 
-instance Position QuantumPosition QuantumMove QuantumParams where
-  initialPosition (QuantumParams sx sy seed) =
-    QuantumPosition sx sy [initialPosition (ClassicParams sx sy)] (mkStdGen seed)
-  checkWinner p = asum $ checkWinner <$> classicPositions p
+instance (Position pos m par, Ord m) =>
+         Position (QuantumPosition pos) (QuantumMove m) (QuantumParams par) where
+  initialPosition (QuantumParams par seed) = QuantumPosition [initialPosition par] (mkStdGen seed)
+  checkWinner p = asum $ checkWinner <$> positions p
   makeMove p player (ClassicMove c) = makeQuantumMove p player [c]
   makeMove _ _ (QuantumMove c1 c2)
     | c1 == c2 = Nothing
@@ -37,25 +33,25 @@ instance Position QuantumPosition QuantumMove QuantumParams where
   generateMoves p =
     fmap ClassicMove moves ++ [QuantumMove c1 c2 | c1 <- moves, c2 <- moves, c1 < c2]
     where
-      sx = boardSizeX p
-      sy = boardSizeY p
-      moves = [c | x <- [0 .. sx - 1], y <- [0 .. sy - 1], let c = Coord x y, isAny p c isNothing]
+      moves = concatMap generateMoves $ positions p
 
-isAny :: QuantumPosition -> Coord -> (Cell -> Bool) -> Bool
-isAny p c cond = any (cond . (`getAt` c) . getMatrix) (classicPositions p)
-
-makeQuantumMove :: QuantumPosition -> Player -> [Coord] -> Maybe QuantumPosition
+makeQuantumMove ::
+     Position pos move params
+  => QuantumPosition pos
+  -> Player
+  -> [move]
+  -> Maybe (QuantumPosition pos)
 makeQuantumMove p player cs =
-  case partition (cond . getMatrix) (classicPositions p) of
+  case partition splitByEmptiness (positions p) of
     -- Nowhere to place
     ([], _) -> Nothing
     -- Absolutely empty cell
-    (empties, []) -> Just $ p {classicPositions = newPositions empties}
+    (empties, []) -> Just $ p {positions = newPositions empties}
     -- Partially empty cell, decoherence occures
     (empties, occupied) ->
       Just
         $ p
-            { classicPositions =
+            { positions =
                 if canPlace
                   then newPositions empties
                   else occupied
@@ -64,5 +60,5 @@ makeQuantumMove p player cs =
       where
         (canPlace, rnd') = uniform $ rnd p
   where
-    cond m = and [isNothing (getAt m c) | c <- cs]
+    splitByEmptiness pos = all (isNothing . makeMove pos player) cs
     newPositions empties = concat [fmap (\p' -> fromJust $ makeMove p' player c) empties | c <- cs]
